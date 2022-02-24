f956ac735f26003afd5f18d5669849b0427fe9fc
Jason Serafica <seraficaj@gmail.com>
2022-02-24 12:18:34 -0800

    eager loading

--- one-many.js
+++ one-many.js
@@ -6,21 +6,31 @@ const relationshipCrud = async () => {
     try {
         // CREATE
             //find a user
-            const foundUser = await db.user.findOne({
-                where: {
-                    firstName: 'Jason'
-                }
-            }) // finds first available user
+            // const foundUser = await db.user.findOne({
+            //     where: {
+            //         firstName: 'Jason'
+            //     }
+            // }) // finds first available user
             // give him/her a pet
-            const petMongoose = await foundUser.createPet({
-                name: 'Stella',
-                description: 'The Spoiled Mongoose',
+            // const petMongoose = await foundUser.createPet({
+            //     name: 'Stella',
+            //     description: 'The Spoiled Mongoose',
 
-            });
-            console.log(petMongoose);
+            // });
+            // console.log(petMongoose);
         // READ
-        const mongooseOwner = await petMongoose.getUser()
-        console.log(mongooseOwner);
+        // const mongooseOwner = await petMongoose.getUser()
+        // const pets = await foundUser.getPets();
+        // console.log(pets);
+
+        // Eager Loading
+        const eagerLoaded = await db.user.findOne({
+            where: {
+                firstName: 'Jason'
+            },
+            include: [db.pet] // supply an array of models
+        })
+        console.log(eagerLoaded.pets);
         // UPDATE
     }
     catch(err) {
